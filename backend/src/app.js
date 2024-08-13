a7c8c4f5aab18c5a2c1d4b2f34c728fb3aa0e5a7
ShuklaProgrammer <thouser346@gmail.com>
2024-08-13 17:13:28 +0530

    frustrated on again changing the cors origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static("public"))
 app.use(cors({
-    origin: "http://localhost:5173", //https://shopkart-ecommerce.vercel.app
+    origin: "https://shopkart-ecommerce.vercel.app", //http://localhost:5173
     credentials: true,
 }))
 app.use(cookieParser())
