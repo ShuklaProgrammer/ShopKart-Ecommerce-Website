a0ae6c414e1e6ae42248724306028cd9fb7b4104
ShuklaProgrammer <thouser346@gmail.com>
2024-08-31 21:54:23 +0530

    mobile responsive in trackorder.jsx

--- frontend/ShopKart/src/pages/User/TrackOrder.jsx
+++ frontend/ShopKart/src/pages/User/TrackOrder.jsx
@@ -57,14 +57,14 @@ const TrackOrder = () => {
   })
 
   return (
-    <section className='flex justify-center mx-10 my-10'>
+    <section className='flex justify-center sm:mx-10 my-10'>
       <form onSubmit={formik.handleSubmit} className='w-[90%] space-y-4'>
         <h1 className='text-2xl font-semibold'>Track Order</h1>
-        <p className='w-[60vw] text-gray-500'>To track your order please enter your order ID in the input field below and press the “Track Order” button. this was given to you on your receipt and in the confirmation email you should have received.</p>
-        <div className='flex gap-10'>
+        <p className='text-gray-500'>To track your order please enter your order ID in the input field below and press the “Track Order” button. this was given to you on your receipt and in the confirmation email you should have received.</p>
+        <div className='sm:flex items-center gap-10'>
         <div className='space-y-2'>
           <span>Order ID</span>
-          <Input id="orderId" value={formik.values.orderId} onChange={formik.handleChange} className="border-2 w-[30vw]" placeholder="ID..."/>
+          <Input id="orderId" value={formik.values.orderId} onChange={formik.handleChange} className="border-2 sm:w-[30vw]" placeholder="ID..."/>
           {formik.touched.orderId && formik.errors.orderId ? (
             <div className='text-red-500 text-sm'>{formik.errors.orderId}</div>
           ) : (
@@ -73,7 +73,7 @@ const TrackOrder = () => {
         </div>
         <div className='space-y-2'>
           <span>Delivery Email</span>
-          <Input id="deliveryEmail" value={formik.values.deliveryEmail} onChange={formik.handleChange} className="border-2 w-[30vw]" placeholder="Email address"/>
+          <Input id="deliveryEmail" value={formik.values.deliveryEmail} onChange={formik.handleChange} className="border-2 sm:w-[30vw]" placeholder="Email address"/>
           {formik.touched.deliveryEmail && formik.errors.deliveryEmail ? (
             <div className='text-red-500 text-sm'>{formik.errors.deliveryEmail}</div>
           ) : (
@@ -81,7 +81,7 @@ const TrackOrder = () => {
           )}
         </div>
         </div>
-        <p className='flex items-center gap-2'><AiOutlineInfoCircle/>Order ID that we sended to your in your email address.</p>
+        <p className='flex items-center gap-2'><AiOutlineInfoCircle className='text-2xl'/>Order ID that we sended to your in your email address.</p>
         <Button type="submit" onClick={handleTrackOrder} disabled={formik.isSubmitting} variant="shop" className="px-5 py-3 rounded">{formik.isSubmitting ? <span className='flex items-center gap-2'>Tracking...<Loader size='2em' topBorderSize='0.2em' center={false} fullScreen={false}/><IoMdArrowForward className='text-lg'/></span> : <span className='flex items-center gap-2'>Track Order<IoMdArrowForward className='text-lg'/></span>}</Button>
       </form>
     </section>
