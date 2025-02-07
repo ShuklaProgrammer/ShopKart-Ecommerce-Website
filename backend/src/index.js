f56213e462eb26149cf36718e47c50837fa03a29
ShuklaProgrammer <shuklaprogrammer@gmail.com>
2025-02-07 14:17:56 +0530

    index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,10 +5,12 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: "/env" });
 
-// CronJob.start()
-
 connectDatabase()
   .then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server is running: ${process.env.PORT}`);
+    });
+
     app.get("/", (req, res) => {
       res.send("Hello from server");
     });
